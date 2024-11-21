987ffe20f3cd77766969e33f5ad6b2f8fd15a8f6
Maxim <maksberezyk@gmail.com>
2024-11-21 18:22:01 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,17 @@
+const ownerKey = "qwerty"
+const url = "https://thread-apricot-index.glitch.me/cards/"
+
+fetch(url + ownerKey).then(async function (response) {
+    console.log(await response.json())
+})
+
+
+document.querySelector("#new").addEventListener("click", () => {
+    fetch(url, {
+        method: "POST",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify({"ownerKey" : ownerKey}),
+    }).then(async function (response) {
+        console.log(await response.json())
+    })
+})
